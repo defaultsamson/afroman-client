5188b7e355b1f7825a604a0fd14ed0d45bdc9586
Samson Close <stanmoon95@gmail.com>
2016-08-01 21:42:49 -0400

    Stepped Version Number

--- src/ca/afroman/client/ClientGame.java
+++ src/ca/afroman/client/ClientGame.java
@@ -68,7 +68,7 @@ public class ClientGame extends DynamicTickRenderThread
 	public static final int HEIGHT = WIDTH / 16 * 9;
 	public static final int SCALE = 3;
 	public static final String NAME = "Cancer: The Adventures of Afro Man";
-	public static final short VERSION = 30;
+	public static final short VERSION = 31;
 	public static final BufferedImage ICON = Texture.fromResource(AssetType.INVALID, "icon/32x.png").getImage();
 	
 	public static final int RECEIVE_PACKET_BUFFER_LIMIT = 128;
