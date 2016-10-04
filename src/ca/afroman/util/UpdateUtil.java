60e1334fd1a156c564decdff43cf6197073e9814
qwertysam <stanmoon95@gmail.com>
2016-10-04 10:46:23 -0400

    What the shit, git

--- src/ca/afroman/util/UpdateUtil.java
+++ src/ca/afroman/util/UpdateUtil.java
@@ -3,7 +3,6 @@ package ca.afroman.util;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.IOException;
 import java.net.URL;
 import java.nio.channels.Channels;
 import java.nio.channels.ReadableByteChannel;
@@ -218,6 +217,4 @@ public class UpdateUtil
 			}
 		}
 	}
-	
-	/**
 }
