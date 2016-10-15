9e45b2c6e3e649bb31f98e1dec995e7b3c91cbd8
RCwyatt <rcwyatt917@gmail.com>
2016-10-15 18:43:50 -0400

    Redone Doors +doors updated to fit new walls

--- src/ca/afroman/assets/Assets.java
+++ src/ca/afroman/assets/Assets.java
@@ -199,11 +199,11 @@ public class Assets
 		
 		assets.add(sheet.getSubTexture(AssetType.TILE_BRIDGE_WOOD_SIDE, 16 * 7, 16 * 7, 24, 24));
 		
-		assets.add(sheet.getSubTexture(AssetType.TILE_DOOR_FRONT_CLOSED, 16 * 7, 16 * 5, 16, 16));
-		assets.add(sheet.getSubTexture(AssetType.TILE_DOOR_FRONT_OPEN, 16 * 8, 16 * 5, 16, 16));
-		assets.add(sheet.getSubTexture(AssetType.TILE_DOOR_SIDE_CLOSED, 16 * 10, 16 * 5, 16, 32));
-		assets.add(sheet.getSubTexture(AssetType.TILE_DOOR_SIDE_OPEN_TOP, 16 * 9, 16 * 5, 16, 24));
-		assets.add(sheet.getSubTexture(AssetType.TILE_DOOR_SIDE_OPEN_BOTTOM, 16 * 11, (16 * 6), 16, 16));
+		assets.add(sheet.getSubTexture(AssetType.TILE_DOOR_FRONT_CLOSED, 16 * 7, 16 * 0, 48, 16));
+		assets.add(sheet.getSubTexture(AssetType.TILE_DOOR_FRONT_OPEN, 16 * 7, 16 * 1, 48, 16));
+		assets.add(sheet.getSubTexture(AssetType.TILE_DOOR_SIDE_CLOSED, 16 * 10, 16 * 0, 16, 48));
+		assets.add(sheet.getSubTexture(AssetType.TILE_DOOR_SIDE_OPEN, 16 * 11, 16 * 0, 16, 48));
+		
 		
 		assets.add(new StepSpriteAnimation(new int[] { 0, 1 }, AssetType.TILE_PLATE_TEST, false, 30, sheet.getSubTexture(AssetType.TILE_PLATE_TEST, 16 * 10, 16 * 3, 16, 32).toTextureArray(1, 2)));
 		
